bcbc70383cf2f16d88fd284665d7206078f3739a
Yangunli <yangun410@gmail.com>
2023-01-16 12:27:59 +0800

    Fix: Modify logic about next year

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -1,26 +1,24 @@
-const newYears = "1 Jan 2023";
-const daysEl = document.getElementById("days")
-const hoursEl = document.getElementById("hours")
-const minutesEl = document.getElementById("minutes")
-const secondsEl = document.getElementById("seconds")
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minutesEl = document.getElementById("minutes");
+const secondsEl = document.getElementById("seconds");
 
-function countdowm(){
-    const newYearDate = new Date(newYears);
-    const currentDate = new Date();
-    // const totalSeconds = (newYearDate -currentDate) /1000
+function countdowm() {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const newYearDate = new Date(`1 Jan ${currentYear + 1}`);
+  // const totalSeconds = (newYearDate -currentDate) /1000
 
-    const days = Math.floor((newYearDate -currentDate) /1000/60/60/24)
-    const hours =  Math.floor((newYearDate -currentDate) /1000/60/60%24)
-    const minutes = Math.floor((newYearDate -currentDate) /1000/60%60)
-    const seconds = Math.floor((newYearDate -currentDate) /1000%60)
-  
-    daysEl.innerHTML= days;
-    hoursEl.innerHTML= hours;
-    minutesEl.innerHTML= minutes;
-    secondsEl.innerHTML= seconds;
+  const days = Math.floor((newYearDate - currentDate) / 1000 / 60 / 60 / 24);
+  const hours = Math.floor(((newYearDate - currentDate) / 1000 / 60 / 60) % 24);
+  const minutes = Math.floor(((newYearDate - currentDate) / 1000 / 60) % 60);
+  const seconds = Math.floor(((newYearDate - currentDate) / 1000) % 60);
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minutesEl.innerHTML = minutes;
+  secondsEl.innerHTML = seconds;
 }
 
-
 countdowm();
-setInterval(countdowm,1000)
-
+setInterval(countdowm, 1000);
